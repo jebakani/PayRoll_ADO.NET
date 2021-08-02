f79c9fda70677b67d0703eb57589897a0c6a04e5
Jebakani <vjishwarya@gmail.com>
2021-08-02 19:02:41 +0530

    [JEBAKANI] Creating the Payroll data base and implementing using c#

---
+++ PayRollWithDatabase/EmployeeRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayRollWithDatabase
+{
+    class EmployeeRepo
+    {
+        /// <summary>
+        /// Creating the connection
+        /// </summary>
+        public static string connectionString = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=Employee_Payroll_Database";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+        public void GetAllData()
+        {
+
+        }
+    }
+}
